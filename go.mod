05db3b1a7e2750de7c45d04c1e647708bc5ad6f4
Pieter Claerhout <pieter@yellowduck.be>
2019-07-29 12:21:34 +0200

    Added the PendingCount function

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/pieterclaerhout/go-waitgroup
+
+go 1.12
--- waitgroup.go
+++ waitgroup.go
@@ -2,12 +2,14 @@ package waitgroup
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 // WaitGroup implements a simple goruntine pool.
 type WaitGroup struct {
 	size      int
 	pool      chan byte
+	waitCount int64
 	waitGroup sync.WaitGroup
 }
 
@@ -30,6 +32,7 @@ func (wg *WaitGroup) BlockAdd() {
 		wg.pool <- 1
 	}
 	wg.waitGroup.Add(1)
+	atomic.AddInt64(&wg.waitCount, 1)
 }
 
 // Done pops ‘one’ out the group.
@@ -38,9 +41,15 @@ func (wg *WaitGroup) Done() {
 		<-wg.pool
 	}
 	wg.waitGroup.Done()
+	atomic.AddInt64(&wg.waitCount, -1)
 }
 
 // Wait waiting the group empty
 func (wg *WaitGroup) Wait() {
 	wg.waitGroup.Wait()
+}
+
+// PendingCount returns the number of pending operations
+func (wg *WaitGroup) PendingCount() int64 {
+	return wg.waitCount
 }
